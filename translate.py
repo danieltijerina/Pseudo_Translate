49f0f5af6b2ee08fc243af853f69379a40f04e3a
Mauricio Guadiana <mauriciogm97@hotmail.com>
2018-08-25 18:04:19 -0500

    added support for conditions

--- translate.py
+++ translate.py
@@ -4,31 +4,56 @@ import shlex
 
 current_line = 0
 
+indent = ''
+
 my_file = open(sys.argv[1], 'r')
 language = sys.argv[2]
 output_file = open("output.cpp", 'w')
 content = my_file.readlines()
 
 def endfile() :
-        output_file.write('}\n')
+	output_file.write('}\n')
 
 def startfile():
-        output_file.write('#include <iostream>\n')
-        output_file.write('using namespace std;\n\n')
+	output_file.write('#include <iostream>\n')
+	output_file.write('using namespace std;\n\n')
 	output_file.write('int main() {\n')
 
 def makeprintcpp(printOutput):
 	printOutput.pop(0)
-        output_string = "cout  "
-        for word in printOutput:
-                output_string += "<<"
-                output_string += word
-        output_string += ";"
-        output_file.write(output_string)
+	output_string = "cout  "
+	for word in printOutput:
+		output_string += "<<"
+		output_string += word
+	output_string += ";"
+	output_file.write(output_string)
 	output_file.write('\n')
 
 def makeforcpp(words_in_line):
 	print(words_in_line)
+
+def makeIfcpp(words_in_line):
+	words_in_line.pop(0)
+	global indent
+	output_string = indent + "if ("
+	for word in words_in_line:
+		output_string += word + " "
+	output_string += ") { \n"
+	output_file.write(output_string+"\n")
+	indent += "\t"
+
+def makeElsecpp(words_in_line):
+	global indent
+	indent = indent[0:len(indent)-3]
+	output_string = indent + "} else { \n"
+	output_file.write(output_string+"\n")
+	indent += "\t"
+
+def makeEndIfcpp(words_in_line):
+	global indent
+	indent = indent[0:len(indent)-3]
+	output_string = indent + "} \n"
+	output_file.write(output_string+"\n")
 
 def nextline():
 	global current_line
@@ -47,9 +72,15 @@ cpp_reserved = {
         # print tokens
         'var' : 'auto',
         'print' : makeprintcpp,
-        ',' : '<<'
+        ',' : '<<',
 
         # loop tokens
+
+        # condition tokens
+	'if' : makeIfcpp,
+        'else' : makeElsecpp,
+        #'elif' : makeElseIfcpp,
+        'endif' : makeEndIfcpp,
 
 }
 
