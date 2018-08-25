525c8d5b449f9cc4f8d1190053247834036f46ef
Roberto <roberto1897@gmail.com>
2018-08-25 16:53:18 -0500

    Added functions readline and checktoken to make a base of the program. Also moved the reserved.py dictionary into the translate.py file

--- translate.py
+++ translate.py
@@ -1,6 +1,13 @@
 import sys
-#from reserved import py, cpp 
+#from reserved import py_reserved, cpp_reserved 
 import shlex
+
+current_line = 0
+
+my_file = open(sys.argv[1], 'r')
+language = sys.argv[2]
+output_file = open("output.cpp", 'w')
+content = my_file.readlines()
 
 def endfile() :
         output_file.write('}\n')
@@ -11,6 +18,7 @@ def startfile():
 	output_file.write('int main() {\n')
 
 def makeprint(printOutput):
+	printOutput.pop(0)
         output_string = "cout  "
         for word in printOutput:
                 output_string += "<<"
@@ -19,13 +27,36 @@ def makeprint(printOutput):
         output_file.write(output_string)
 	output_file.write('\n')
 
-my_file = open(sys.argv[1], 'r')
-output_file = open("output.cpp", 'w')
-startfile()
-content = my_file.readlines()
-for line in content:
+def nextline():
+	global current_line
+	if current_line >= len(content):
+		return "-1"
+	line = content[current_line]
+	current_line += 1
 	words = shlex.split(line, posix=False)
-	if words[0] == "print":
-		words.pop(0)
-		makeprint(words)
+	return words
+
+def checkToken(words_in_line):
+	if language == 'c++':
+		cpp_reserved[words_in_line[0]](words_in_line)
+		
+cpp_reserved = {
+        # print tokens
+        'var' : 'auto',
+        'print' : makeprint,
+        ',' : '<<'
+
+        # loop tokens
+
+}
+
+py_reserved = {
+        'print' : 'print(',
+        ',' : '+',
+}		
+
+startfile()
+while current_line < len(content):
+	words_in_line  = nextline()
+	checkToken(words_in_line)
 endfile()
