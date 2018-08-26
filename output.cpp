4cdb01d8bf2385c362241dbd5910dce84c53a488
Roberto <roberto1897@gmail.com>
2018-08-25 19:22:42 -0500

    Modified outuput.cpp as a result of running the script

--- output.cpp
+++ output.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+using namespace std;
+
+void imprimir() {
+for(int x = 2; x <= 10; x++) {
+	cout  << x;
+}
+for(int y = 5; y <= 15; y++) {
+	cout  << y;
+}
+}
+
 int main() {
-cout  <<"Hello World";
+imprimir() ;
 }
