8d571df940c4cbba33be9d0faaed8c66e701538a
Roberto <roberto1897@gmail.com>
2018-08-26 01:57:37 -0500

    Added pyton script to transform the json graph into a pseudo code for the translate script

---
+++ flowToPseudo.py
@@ -0,0 +1,24 @@
+import json
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from pprint import pprint
+	
+with open('flowchart.json') as f:
+    data = json.load(f)
+G = nx.DiGraph()
+
+Nodes_all = data[u'nodes']
+for node in Nodes_all:
+	G.add_node(node[u'id'], info=node[u'text'])
+Edges = data[u'edges']
+for edge in Edges:
+	G.add_edge(edge[u'source'], edge[u'target'], label=edge[u'label'])
+#nx.draw(G, with_labels=True, font_weight='bold')
+#plt.show()
+print(G.nodes[u'start'])
+print(G[u'question1'])
+#print(G)
+#pprint(Edges)
+
+#pprint(data)
